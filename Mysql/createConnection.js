0f3b7727a1f7188560d0a5b71fbfa68fb6e1b547
engineve <yeindap@gmail.com>
2020-11-01 16:27:33 +0900

    Update createConnection.js
    create user, privilege setting

--- Mysql/createConnection.js
+++ Mysql/createConnection.js
@@ -16,8 +16,14 @@ connection.query('SELECT * FROM tables', function(error, results) {
 
 module.exports = connection;
 
- "Client does not support authentication protocol requested by server; consider upgrading MySQL client"
+//  "Client does not support authentication protocol requested by server; consider upgrading MySQL client"
 
--> terminal : alter user 'root'@'localhost' identified with mysql_native_password by 'password';
+// -> terminal : alter user 'root'@'localhost' identified with mysql_native_password by 'password';
 
-mysql -u root -p < server/schema.sql
+// mysql -u root -p < server/schema.sql
+
+// SELECT Host.User FROM mysql.user;
+
+// CREATE USER 'user1'@'%' IDENTIFIED BY '111';    users1 any domain with password 111
+
+// GRANT ALL PRIVILEGS ON database.tables TO 'user1'@'%';
